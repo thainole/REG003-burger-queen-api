791cfeb0207534576fcd773d073e9a0a73765158
Thais Illescas <thainole@gmail.com>
2021-08-12 13:15:20 -0500

    creating user

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,8 @@
 const User = require('../models/user');
 
 /* Post admin User */
-const postAdminUser = (adminUser, next ) => {
+const postAdminUser = (adminUser, next) => {
+
   const userFind = User.findOne({ email: adminUser.email });
 
   userFind.then((doc) => {
@@ -19,10 +20,43 @@ const postAdminUser = (adminUser, next ) => {
         console.info('Ha ocurrido un error', err);
       }
     });
-}
+};
+
+
+// CREANDO USUARIOS
+const postUsers = async (req, resp, next) => {
+
+  const { email, password, roles } = req.body;
+  const user = new User({ email, password, roles });
+
+  // Verificamos si mandan correo y contraseña
+  if (user.email === '' || user.password === '') {
+    return next(400);
+  }
+
+  // Verificamos si el email es válido
+  /* if (!user.email.isEmail()) {
+    return next(400);
+  } */
+
+  // Verificamos si el correo existe
+  const existingEmail = await User.findOne({ email });
+  if (existingEmail) {
+    return next(403);
+  }
+
+  // Guardar en database
+  await user.save();
+  resp.json({
+    user,
+  });
+};
+
+
 
 module.exports = {
   getUsers: (req, resp, next) => {
   },
-  postAdminUser
+  postAdminUser,
+  postUsers
 };
