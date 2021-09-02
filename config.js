6292904a85aaec4cb2b2d3e050af13d02cc96104
mairelis-montilla <mairelismontilla@gmail.com>
2021-09-02 09:52:15 -0500

    fix env

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.dbUrl = process.env.DB_URL || 'mongodb+srv://bq:Holasomosteam@cluster0.nms3e.mongodb.net/test';
-exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
-exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
+exports.port = process.argv[2] || process.env.PORT;
+exports.dbUrl = process.env.DB_URL;
+exports.secret = process.env.JWT_SECRET;
+exports.adminEmail = process.env.ADMIN_EMAIL;
+exports.adminPassword = process.env.ADMIN_PASSWORD;
